341b490f798cc1210c579ec2ce2e2b30a0d8e7c1
jeremywallace <Jeremy Wallace>
2014-08-22 16:27:12 -0700

    Fix

--- routes/views/create.js
+++ routes/views/create.js
@@ -1,6 +1,5 @@
 var keystone = require('keystone'),
 	Enquiry = keystone.list('Enquiry'),
-	feed = require("feed-read"),
 	share = require('share').client;
 
 exports = module.exports = function(req, res) {
@@ -8,6 +7,22 @@ exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res),
 		locals = res.locals;
 	
+	// Open the 'hello' document, which should have type 'text':
+	client.open('hello', 'text', {host: 'localhost', port: 8000}, function(doc, error) {
+	    
+		// Insert some text at the start of the document (position 0):
+	    doc.submitOp({i:"Hi there!\n", p:0});
+
+	    // Get the contents of the document for some reason:
+	    console.log(doc.snapshot);
+
+	    doc.on('change', function(op) {
+	        console.log('Version: ' + doc.version);
+	    });
+
+	    // Close the doc if you want your node app to exit cleanly
+	    // doc.close();
+	    
 	// Set locals
 	locals.section = 'onthewire';
 	view.render('onthewire');
