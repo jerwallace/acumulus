b24723d14ab548f1e627623c116b33ef5cefbb42
jeremywallace <Jeremy Wallace>
2014-08-25 14:08:41 -0700

    Testing

--- routes/views/create.js
+++ routes/views/create.js
@@ -1,27 +1,15 @@
 var keystone = require('keystone'),
 	Enquiry = keystone.list('Enquiry'),
-	client = require('share').client;
+	ace = require('ace');
 
 exports = module.exports = function(req, res) {
 	
 	var view = new keystone.View(req, res),
 		locals = res.locals;
 
-	// Open the 'hello' document, which should have type 'text':
-	client.open('hello', 'text', {host: '127.0.0.1', port: 8000}, function(doc, error) {
-	    // Insert some text at the start of the document (position 0):
-	    doc.submitOp({i:"Hi there!\n", p:0});
-
-	    // Get the contents of the document for some reason:
-	    console.log(doc.snapshot);
-
-	    doc.on('change', function(op) {
-	        console.log('Version: ' + doc.version);
-	    });
-
-	    // Close the doc if you want your node app to exit cleanly
-	    // doc.close();
-	});
+	editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/javascript");
 	
 	// Set locals
 	locals.section = 'create';
